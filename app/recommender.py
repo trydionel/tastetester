4870e70774883701b8a391a7a3f9592216afe68b
Jeff Tucker <trydionel@gmail.com>
2026-06-10 22:23:54 +0000

    Add a very basic streamlit app

--- app/recommender.py
+++ app/recommender.py
@@ -1,5 +1,7 @@
 import logging
 from pathlib import Path
+from typing import Any
+from pydantic import BaseModel
 from sklearn.model_selection import train_test_split
 import xgboost as xgb
 import optuna
@@ -8,6 +10,13 @@ import pandas as pd
 
 from etl.common.audio_features import fetch_audio_features, fetch_genres, fetch_track_details
 from etl.common.vectorstore import ListeningVectorStore
+
+class TrackAnalysis(BaseModel):
+  details: dict[str, Any]
+  matches: list[dict[str, Any]]
+  vector: list[float]
+  expected_plays: float
+  percentile: float
 
 class Recommender():
   def __init__(self, random_state = None):
@@ -56,10 +65,9 @@ class Recommender():
     model.save_model(self._predicted_listens_model_path)
     return model
   
-  def analyze(self, track):
+  def analyze(self, track) -> TrackAnalysis:
     logging.debug(f"Fetching track details for {track}")
     track_details = fetch_track_details(track)
-    # details = {'content': [{'id': '27ff0fda-0e24-45ef-bb95-d08d3c55200b', 'trackTitle': 'The Motherload', 'artists': [{'id': '4e8883d1-2066-4a14-85a4-491206dac463', 'name': 'Mastodon', 'href': 'https://open.spotify.com/artist/1Dvfqq39HxvCJ3GvfeIFuT'}], 'durationMs': 299786, 'isrc': 'USRE11400112', 'ean': None, 'upc': None, 'href': 'https://open.spotify.com/track/6EF0xhfKtQNqUPz2mnE5BD', 'availableCountries': 'AR,AU,AT,BE,BO,BR,BG,CA,CL,CO,CR,CY,CZ,DK,DO,DE,EC,EE,SV,FI,FR,GR,GT,HN,HK,HU,IS,IE,IT,LV,LT,LU,MY,MT,MX,NL,NZ,NI,NO,PA,PY,PE,PH,PL,PT,SG,SK,ES,SE,CH,TW,TR,UY,US,GB,AD,LI,MC,ID,JP,TH,VN,RO,IL,ZA,SA,AE,BH,QA,OM,KW,EG,MA,DZ,TN,LB,JO,PS,IN,BY,KZ,MD,UA,AL,BA,HR,ME,MK,RS,SI,BD,PK,LK,GH,KE,NG,TZ,UG,AG,AM,BS,BB,BZ,BW,BF,CV,CW,DM,FJ,GM,GD,GW,HT,JM,LS,LR,MW,ML,FM,NA,NE,PG,PR,SM,ST,SN,SC,SL,KN,LC,VC,TL,TT,AZ,BN,BI,KH,CM,TD,KM,GQ,SZ,GA,GN,KG,LA,MO,MR,MN,NP,RW,TG,UZ,ZW,BJ,MG,MU,MZ,AO,CI,DJ,ZM,CD,CG,IQ,LY,TJ,VE,ET,XK', 'popularity': 54}]}
     if len(track_details['content']) == 0:
       track_details = {}
     else:
@@ -68,7 +76,6 @@ class Recommender():
 
     logging.debug(f"Fetching audio features for {track}")
     features = fetch_audio_features([track])
-    # features = {'content': [{'id': '27ff0fda-0e24-45ef-bb95-d08d3c55200b', 'href': 'https://open.spotify.com/track/6EF0xhfKtQNqUPz2mnE5BD', 'isrc': 'USRE11400112', 'acousticness': 7.1e-06, 'danceability': 0.363, 'energy': 0.971, 'instrumentalness': 0.00358, 'key': 5, 'liveness': 0.0797, 'loudness': -2.974, 'mode': 0, 'speechiness': 0.0776, 'tempo': 146.018, 'valence': 0.347}]}
     if len(features['content']) == 0:
       audio_features = {}
     else:
@@ -103,7 +110,7 @@ class Recommender():
     analysis["percentile"] = self._store.total_plays_ecdf(analysis["expected_plays"])
     logging.debug(f"Expected number of plays for {analysis['details']['artists'][0]['name']} - {analysis['details']['trackTitle']}: {analysis['expected_plays']:.2f} ({analysis['percentile']:.2f} percentile)")
 
-    return analysis
+    return TrackAnalysis(**analysis)
 
   def _predict_listens(self, analysis):
     model = self._predicted_listens_model()
---
+++ streamlit_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from app import recommender
+from app.recommender import Recommender
+
+st.title("Taste tester")
+
+spotify_url = st.text_input("Enter a track URL to analyze")
+track_uri = spotify_url.split("/")[-1].split("?")[0]
+
+if not track_uri:
+    st.stop()
+
+st.iframe(f"https://open.spotify.com/embed/track/{track_uri}")
+
+try:
+    with st.spinner("Analyzing track..."):
+        recommender = Recommender()
+        analysis = recommender.analyze(track_uri)
+    
+    st.subheader("Analysis")
+    st.write(f"Expected number of plays for {analysis.details['artists'][0]['name']} - {analysis.details['trackTitle']}: {analysis.expected_plays:.2f} ({analysis.percentile:.2f} percentile)")
+    
+    st.subheader("Most similar listening history")
+    for match in analysis.matches:
+        st.write(f"{match['artist_name']} - {match['track_name']} ({match['total_plays']} listens, {match['distance']:.2f} distance)")
+except:
+    st.write("Unable to analyze song")
