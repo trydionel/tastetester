8184a7a37e0a037cc7e646417c0179351fa8ad55
Jeff Tucker <trydionel@gmail.com>
2026-06-12 21:14:23 +0000

    Trying to get Prefect to connect to PostgreSQL...

--- terraform/modules/cloud_sql/outputs.tf
+++ terraform/modules/cloud_sql/outputs.tf
@@ -1,11 +1,15 @@
+locals  {
+  private_ip = try(google_sql_database_instance.postgres.ip_address[0].ip_address, "")
+}
+
 output "connection_name" {
   value = google_sql_database_instance.postgres.connection_name
 }
 
 output "private_ip" {
-  value = try(google_sql_database_instance.postgres.ip_address[0].ip_address, "")
+  value = local.private_ip
 }
 
 output "connection_string" {
-  value = "postgresql://${google_sql_user.db_user.name}:${google_sql_user.db_user.password}@/${google_sql_database.db.name}?host=/cloudsql/${google_sql_database_instance.postgres.connection_name}"
+  value = "postgresql+asyncpg://${google_sql_user.db_user.name}:${google_sql_user.db_user.password}@${local.private_ip}:5432/${google_sql_database.db.name}?host=/cloudsql/${google_sql_database_instance.postgres.connection_name}"
 }
